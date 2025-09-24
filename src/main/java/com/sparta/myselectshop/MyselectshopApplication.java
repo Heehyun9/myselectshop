c9c17cad661796fbedf5b07eab2370abb07ab175
노희현 <81168262+Heehyun9@users.noreply.github.com>
2025-09-24 12:58:32 +0900

    Scheduler 기능 구현

--- src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
+++ src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
@@ -3,7 +3,9 @@ package com.sparta.myselectshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling  //scheduler 사용(주기적 업데이트)
 @EnableJpaAuditing  //timestamp 활용
 @SpringBootApplication
 public class MyselectshopApplication {
